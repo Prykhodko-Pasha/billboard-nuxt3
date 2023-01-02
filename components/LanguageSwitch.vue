3422c8ba407a14e4986d3f8c3b7569610f0a0c7c
Prykhodko-Pasha <prykhodko.p@gmail.com>
2023-01-02 13:58:45 +0200

    init

---
+++ components/LanguageSwitch.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ul>
+      <li><button>EN</button></li>
+      <li><button>UK</button></li>
+      <li><button>DE</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/profile">Profile</NuxtLink></li>
+      <li><NuxtLink to="/login">Login</NuxtLink></li>
+      <li><NuxtLink to="/signup">Signup</NuxtLink></li>
+    </ul>
+    <!-- <LanguageSwitch /> -->
+    <!-- <AppBar /> -->
+    <main class="main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding: 1rem 2rem;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.main {
+  padding: 4rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
